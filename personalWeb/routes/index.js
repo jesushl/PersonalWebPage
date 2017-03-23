d051ac88f1e9ad31e7d2745de9f39f13fe819608
Jesus HL <jesushledon@gmail.com>
2017-03-22 21:48:10 -0600

    Adding title in express '/' GET method

--- personalWeb/routes/index.js
+++ personalWeb/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jose De Jesus Herrera Ledon' });
 });
 
 module.exports = router;
