a0adb0f3042309925da70949ac3db6a5213bc27b
Jesus HL <jesushledon@gmail.com>
2017-03-22 22:53:48 -0600

    Adding correct route and changes for index

--- personalWeb/routes/index.js
+++ personalWeb/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('home', { title: 'Jose De Jesus Herrera Ledon' });
+  res.render('index', { title: 'Jose De Jesus Herrera Ledon' });
 });
 
 module.exports = router;
