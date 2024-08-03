7e7583aecbe6da957b6e80ef84d2537f72061aaa
Jose de Jesus Herrera Ledon <j3su5.pro@gmail.com>
2024-08-02 19:38:50 -0600

    Fixing this miss

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path')
+const app = express();
+
+
 
 app.get('/', function (req, res) {
-    res.sendFile('D:/Users/jose.herrera/Documents/www/PersonalWebPage/home.html');
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
 });
